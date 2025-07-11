fcfcf371337220a619d4fcd9c68c8d14f00c8384
RizanFeroze <rizanferoze@gmail.com>
2025-07-12 02:05:01 +0400

    fix: add local bin path for uvicorn

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-which uvicorn
-uvicorn --version
+echo "🐍 Adjusting PATH so 'uvicorn' can be found"
+export PATH="/opt/render/project/src/.local/bin:$PATH"
 
 echo "🚀 Starting Uvicorn..."
 uvicorn api_server:app --host 0.0.0.0 --port 10000
